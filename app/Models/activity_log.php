473255b78566d23b7a8e76e8a161c2acfed9636c
stephen <stevenmaina17@gmail.com>
2023-06-03 13:07:35 +0300

    activity  update

--- app/Models/activity_log.php
+++ app/Models/activity_log.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class activity_log extends Model
 {
     Protected $table = 'activity_log';
+    protected $guarded = [''];
+   public function user()
+   {
+      return $this->belongsTo(User::class, 'user_code', 'user_code');
+   }
 }
