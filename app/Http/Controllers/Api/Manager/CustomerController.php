cc208cf9b20d22887da46c28f8144c8c723e69f6
stephen <stevenmaina17@gmail.com>
2023-06-09 11:12:08 +0300

    manager  api  update

--- app/Http/Controllers/Api/Manager/CustomerController.php
+++ app/Http/Controllers/Api/Manager/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\manager;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerController extends Controller
 {
@@ -13,7 +14,7 @@ class CustomerController extends Controller
       return response()->json([
          "success" => true,
          "status" => 200,
-         "data" => User::where('account_type', 'Customer')->get(),
+         "data" => User::where('account_type', 'Customer')->where('region_id', Auth::user()->region_id)->get(),
       ]);
    }
 }
