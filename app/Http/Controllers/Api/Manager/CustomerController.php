ee86b7980aba1549ca81a2de8013df5872e7fb1d
stephen <stevenmaina17@gmail.com>
2023-06-10 10:37:13 +0300

    manager  api  update

--- app/Http/Controllers/Api/Manager/CustomerController.php
+++ app/Http/Controllers/Api/Manager/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\manager;
 use App\Http\Controllers\Controller;
 use App\Models\customers;
 use App\Models\User;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,7 +20,12 @@ class CustomerController extends Controller
 //         "data" => customers::where('region_id', Auth::user()->region_id)->get(),
 //      ]);
 
-      $customers = customers::with(['number_visited','orders.orderItems'])
+      $customers = customers::withCount([
+         'number_visited' => function ($query) {
+            $query->select(new Expression('COUNT(*)'))->from('checkins')->whereColumn('customer_id', 'customers.id');
+         }
+      ])
+     ->with(['orders.orderItems'])
          ->select('id', 'customer_name', 'id_number', 'customer_type', 'latitude', 'longitude', 'contact_person', 'telephone', 'is_creditor', 'creditor_approved', 'customer_group', 'customer_secondary_group', 'price_group', 'route', 'route_code', 'region_id', 'subregion_id', 'status', 'email', 'image', 'phone_number', 'business_code', 'created_by', 'updated_by', 'created_at', 'updated_at')
          ->where('region_id', Auth::user()->region_id)
          ->get();
