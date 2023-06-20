4e3e1b377f933209a5b3fba3a4b894033141c914
stephen <stevenmaina17@gmail.com>
2023-06-20 22:20:53 +0300

    distributor order status

--- app/Http/Controllers/Api/customersController.php
+++ app/Http/Controllers/Api/customersController.php
@@ -236,8 +236,7 @@ public function groups(){
    }
    public function add_customer(Request $request)
    {
-      //   $user_code = $request->user()->user_code;
-      $validator           =  Validator::make($request->all(), [
+      $validator = Validator::make($request->all(), [
          "customer_name"   => "required|unique:customers",
          "contact_person"  => "required",
          "business_code"   => "required",
@@ -245,66 +244,53 @@ public function groups(){
          "email"           => "nullable|unique:users",
          "latitude"        => "required",
          "longitude"       => "required",
-         "image" => 'required|image|mimes:jpg,png,jpeg,gif,svg',
+         "image"           => 'required|image|mimes:jpg,png,jpeg,gif,svg',
       ]);
 
       if ($validator->fails()) {
-         return response()->json(
-            [
-               "status" => 401,
-               "message" =>
-               "validation_error",
-               "errors" => $validator->errors()
-            ],
-            403
-         );
+         return response()->json([
+            "status" => 401,
+            "message" => "validation_error",
+            "errors" => $validator->errors()
+         ], 403);
       }
 
-      $random=Str::random(3);
       $image_path = $request->file('image')->store('image', 'public');
-      $emailData = $request->email !== null ? $request->email : $request->customer_name.$random.'@gmail.com';
-      $random=Str::random(20);
-      $route=Routes::where('id', $request->route_code)->first();
-      $subregion=Subregion::where('id', $route->subregion_id)->first();
-      $region=Region::where('id', $subregion->region_id)->first();
-      $user = new User();
-      $user->name = $request->customer_name;
-      $user->email=$emailData;
-      $user->user_code=$random;
-      $user->phone_number = $request->phone_number;
-      $user->gender = $request->gender;
-      $user->account_type= "Customer";
-      $user->email_verified_at =Carbon::now();
-      $user->status="Active";
-      $user->region_id= $region->id ?? Auth::user()->region_id;
-      $user->business_code = Auth::user()->business_code;
-      $user->password = Hash::make("password");
-      $user->save();
-
-      $customer = new customers;
-      $customer->customer_name = $request->customer_name;
-      $customer->contact_person = $request->contact_person;
-      $customer->phone_number = $request->phone_number;
-      $customer->user_code = $user->user_code;
-      $customer->email = $emailData;
-      $customer->address = $request->address;
-      $customer->latitude = $request->latitude;
-      $customer->longitude = $request->longitude;
-      $customer->business_code = $request->business_code;
-      $customer->created_by = $request->user()->user_code;
-      $customer->route_code = $request->route_code;
-      $customer->route = $request->route_code;
-      $customer->customer_group = $request->outlet;
-      $customer->price_group = $request->outlet;
-      $customer->region_id = $region->id;
-      $customer->subregion_id = $subregion->id;
-      $customer->unit_id = $request->route_code;
-      $customer->image = $image_path;
-      $customer->save();
-
-      DB::table('leads_targets')
-         ->where('user_code', $request->user()->user_code)
-         ->increment('AchievedLeadsTarget');
+      $emailData = $request->email ?? $request->customer_name . Str::random(3) . '@gmail.com';
+      $route = Routes::with('subregion.region')->find($request->route_code);
+      $user = User::create([
+         'name' => $request->customer_name,
+         'email' => $emailData,
+         'user_code' => Str::random(20),
+         'phone_number' => $request->phone_number,
+         'gender' => $request->gender,
+         'account_type' => "Customer",
+         'email_verified_at' => Carbon::now(),
+         'status' => "Active",
+         'region_id' => optional($route->subregion->region)->id,
+         'business_code' => Auth::user()->business_code,
+         'password' => Hash::make("password")
+      ]);
+      $customer = customers::create([
+         'customer_name' => $request->customer_name,
+         'contact_person' => $request->contact_person,
+         'phone_number' => $request->phone_number,
+         'user_code' => $user->user_code,
+         'email' => $emailData,
+         'address' => $request->address,
+         'latitude' => $request->latitude,
+         'longitude' => $request->longitude,
+         'business_code' => $request->business_code,
+         'created_by' => $request->user()->user_code,
+         'route_code' => $request->route_code,
+         'route' => $request->route_code,
+         'customer_group' => $request->outlet,
+         'price_group' => $request->outlet,
+         'region_id' => optional($route->subregion->region)->id,
+         'subregion_id' => optional($route->subregion)->id,
+         'unit_id' => $request->route_code,
+         'image' => $image_path
+      ]);
 
       return response()->json([
          "success" => true,
@@ -312,6 +298,85 @@ public function groups(){
          "message" => "Customer added successfully",
       ]);
    }
+
+//   public function add_customer(Request $request)
+//   {
+//      //   $user_code = $request->user()->user_code;
+//      $validator           =  Validator::make($request->all(), [
+//         "customer_name"   => "required|unique:customers",
+//         "contact_person"  => "required",
+//         "business_code"   => "required",
+//         "phone_number"    => "required|unique:customers",
+//         "email"           => "nullable|unique:users",
+//         "latitude"        => "required",
+//         "longitude"       => "required",
+//         "image" => 'required|image|mimes:jpg,png,jpeg,gif,svg',
+//      ]);
+//
+//      if ($validator->fails()) {
+//         return response()->json(
+//            [
+//               "status" => 401,
+//               "message" =>
+//               "validation_error",
+//               "errors" => $validator->errors()
+//            ],
+//            403
+//         );
+//      }
+//
+//      $random=Str::random(3);
+//      $image_path = $request->file('image')->store('image', 'public');
+//      $emailData = $request->email !== null ? $request->email : $request->customer_name.$random.'@gmail.com';
+//      $random=Str::random(20);
+//      $route=Routes::where('id', $request->route_code)->first();
+//      $subregion=Subregion::where('id', $route->subregion_id)->first();
+//      $region=Region::where('id', $subregion->region_id)->first();
+//      $user = new User();
+//      $user->name = $request->customer_name;
+//      $user->email=$emailData;
+//      $user->user_code=$random;
+//      $user->phone_number = $request->phone_number;
+//      $user->gender = $request->gender;
+//      $user->account_type= "Customer";
+//      $user->email_verified_at =Carbon::now();
+//      $user->status="Active";
+//      $user->region_id= $region->id ?? Auth::user()->region_id;
+//      $user->business_code = Auth::user()->business_code;
+//      $user->password = Hash::make("password");
+//      $user->save();
+//
+//      $customer = new customers;
+//      $customer->customer_name = $request->customer_name;
+//      $customer->contact_person = $request->contact_person;
+//      $customer->phone_number = $request->phone_number;
+//      $customer->user_code = $user->user_code;
+//      $customer->email = $emailData;
+//      $customer->address = $request->address;
+//      $customer->latitude = $request->latitude;
+//      $customer->longitude = $request->longitude;
+//      $customer->business_code = $request->business_code;
+//      $customer->created_by = $request->user()->user_code;
+//      $customer->route_code = $request->route_code;
+//      $customer->route = $request->route_code;
+//      $customer->customer_group = $request->outlet;
+//      $customer->price_group = $request->outlet;
+//      $customer->region_id = $region->id;
+//      $customer->subregion_id = $subregion->id;
+//      $customer->unit_id = $request->route_code;
+//      $customer->image = $image_path;
+//      $customer->save();
+//
+//      DB::table('leads_targets')
+//         ->where('user_code', $request->user()->user_code)
+//         ->increment('AchievedLeadsTarget');
+//
+//      return response()->json([
+//         "success" => true,
+//         "status" => 200,
+//         "message" => "Customer added successfully",
+//      ]);
+//   }
 
    public function editCustomer(Request $request)
    {
