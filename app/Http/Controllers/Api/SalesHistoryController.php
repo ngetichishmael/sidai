3faa664d47fabbf0816efbce11951f67a2d35b4c
steven-maina <stevenmaina17@gmail.com>
2023-06-23 12:08:35 +0300

    distributors

--- app/Http/Controllers/Api/SalesHistoryController.php
+++ app/Http/Controllers/Api/SalesHistoryController.php
@@ -15,18 +15,36 @@ class SalesHistoryController extends Controller
       $user_code = $request->user()->user_code;
       //$customerID = $request->customer_id;
 
+//      $query = DB::select('SELECT
+//        `customerID`,
+//        `user_code`,
+//        `supplierID`,
+//        `order_code`,
+//        `price_total`,
+//        `order_status`,
+//        `payment_status`,
+//        `checkin_code`,
+//        `order_type`,
+//        `created_at`
+//    FROM
+//        `orders` where `user_code`=? AND `customerID`=?', [$user_code, $shopID]);
       $query = DB::select('SELECT
-        `customerID`,
-        `user_code`,
-        `order_code`,
-        `price_total`,
-        `order_status`,
-        `payment_status`,
-        `checkin_code`,
-        `order_type`,
-        `created_at`
+        orders.customerID,
+        orders.user_code,
+        distributors.distributor_name,
+        orders.order_code,
+        orders.price_total,
+        orders.order_status,
+        orders.payment_status,
+        orders.checkin_code,
+        orders.order_type,
+        orders.created_at
     FROM
-        `orders` where `user_code`=? AND `customerID`=?', [$user_code, $shopID]);
+        orders
+    INNER JOIN distributors ON orders.supplierID = distributors.id
+    WHERE
+        orders.user_code = ? AND orders.customerID = ?', [$user_code, $shopID]);
+
 
       return response()->json([
          "success" => true,
@@ -39,15 +57,19 @@ class SalesHistoryController extends Controller
       $user_code = $request->user()->user_code;
 
       $vansales = 'Van sales';
-      $query = DB::select(
-         'SELECT
-        *
-    FROM
-        `orders`  where `order_type`=?
-                  AND `user_code`=? AND `customerID`=?',
-         [$vansales, $user_code, $shopID]
-      );
-
+//      $query = DB::select(
+//         'SELECT
+//        *
+//    FROM
+//        `orders`  where `order_type`=?
+//                  AND `user_code`=? AND `customerID`=?',
+//         [$vansales, $user_code, $shopID]
+//      );
+      $query = Orders::with('distributor')
+         ->where('order_type', $vansales)
+         ->where('user_code', $user_code)
+         ->where('customerID', $shopID)
+         ->get();
       return response()->json([
          "success" => true,
          "message" => "Van Sales Order",
@@ -57,7 +79,7 @@ class SalesHistoryController extends Controller
    public function preorder($shopID)
    {
       $query = Orders::where("order_type", 'Pre Order')
-         ->where('customerID', $shopID)
+         ->where('customerID', $shopID)->with('distributor')
          ->get();
       return response()->json([
          "success" => true,
