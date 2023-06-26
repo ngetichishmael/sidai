4e9d4d886ea1850bf833c774e164323a30a7aaf7
steven-maina <stevenmaina17@gmail.com>
2023-06-26 14:40:47 +0300

    orders sidai debug

--- app/Http/Controllers/Api/productsController.php
+++ app/Http/Controllers/Api/productsController.php
@@ -7,6 +7,7 @@ use App\Models\products\product_information;
 use App\Models\Region;
 use App\Models\Subregion;
 use App\Models\UnitRoute;
+use App\Models\warehousing;
 use App\Models\zone;
 use Illuminate\Http\Request;
 
@@ -26,7 +27,9 @@ class productsController extends Controller
    {
       $route_code = $request->user()->route_code;
       $region_id = Region::whereId($route_code)->first();
-      $products = product_information::join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
+      $region = Region::whereId($request->user()->region_id)->first();
+      $warehouse =warehousing::whereIn('region_id',[$region_id,$region ] )->get();
+      $products = product_information::where('warehouse_code',$warehouse->warehouse_code )->join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
          ->join('product_price', 'product_price.productID', '=', 'product_information.id')
          ->select(
             'product_price.branch_id as region',
