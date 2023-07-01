40ff020e9c3b8f39bae6047217574a547d1149a8
Daniel <wisedaniel080@gmail.com>
2023-07-01 16:27:29 +0300

    updates on reports

--- resources/views/partials/stickymenu.blade.php
+++ resources/views/partials/stickymenu.blade.php
@@ -5,59 +5,33 @@
     <div class="col-md-2">
         <div class="card card-inverse">
             <div class="card-body" style="background-color: rgb(46, 45, 45)">
-                <table id="data-table-default" class="table table-striped table-bordered" style="background-color: rgb(46, 45, 45)">
-                    <thead>
-                        <tr>
-                            <th width="20%">Reports</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><a href="{{ route('preorders.reports') }}" style="color: black;font-size:12px">Preorders </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('vansales.reports') }}" style="color: white;font-size:12px">Vansales </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('delivery.reports') }}" style="color: black;font-size:12px">Delivery </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('supplier.reports') }}" style="color: white;font-size:12px">Suppliers </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('distributor.reports') }}" style="color: black;font-size:12px">Distributors
-                                </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('warehouse.reports') }}" style="color: white;font-size:12px">Warehouse </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('regional.reports') }}" style="color: black;font-size:12px">Regional </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('visitation.reports') }}" style="color: white;font-size:12px">Visitation </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('target.reports') }}" style="color: black;font-size:12px">Targets </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('sidai.reports') }}" style="color: white;font-size:12px">Sidai Users </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('clients.reports') }}" style="color: black;font-size:12px">Customers </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('payments.reports') }}" style="color: white;font-size:12px">Payments </a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="{{ route('inventory.reports') }}" style="color: black;font-size:12px">Inventory </a>
-                            </td>
-                        </tr>
-                        </tr>
-                    </tbody>
-                </table>
+               
+                    <a href="{{ route('preorders.reports') }}" style="color: white;font-size:12px">Preorders </a><br><br>
+                    <a href="{{ route('vansales.reports') }}" style="color: white;font-size:12px">Vansales </a><br><br>
+                <a href="{{ route('delivery.reports') }}" style="color: white;font-size:12px">Delivery </a><br><br>
+                        
+                <a href="{{ route('supplier.reports') }}" style="color: white;font-size:12px">Suppliers </a><br><br>
+                
+                <a href="{{ route('distributor.reports') }}" style="color: white;font-size:12px">Distributors
+                        </a><br><br>
+                
+                <a href="{{ route('warehouse.reports') }}" style="color: white;font-size:12px">Warehouse </a><br><br>
+                    
+                
+                <a href="{{ route('regional.reports') }}" style="color: white;font-size:12px">Regional </a><br><br>
+                
+                <a href="{{ route('visitation.reports') }}" style="color: white;font-size:12px">Visitation </a><br><br>
+                    
+                
+                <a href="{{ route('target.reports') }}" style="color: white;font-size:12px">Targets </a><br><br>
+                
+                <a href="{{ route('sidai.reports') }}" style="color: white;font-size:12px">Sidai Users </a><br><br>
+                
+                <a href="{{ route('clients.reports') }}" style="color: white;font-size:12px">Customers </a><br><br>
+            
+                <a href="{{ route('payments.reports') }}" style="color: white;font-size:12px">Payments </a><br><br>
+            
+                <a href="{{ route('inventory.reports') }}" style="color: white;font-size:12px">Inventory </a><br><br>
             </div>
         </div>
     </div>
