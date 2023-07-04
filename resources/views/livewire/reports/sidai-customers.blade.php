e77b0f9ae22ad5eb73ce572f2f643e625edccd3f
Daniel <wisedaniel080@gmail.com>
2023-07-04 14:55:38 +0300

    updates

--- resources/views/livewire/reports/sidai-customers.blade.php
+++ resources/views/livewire/reports/sidai-customers.blade.php
@@ -59,7 +59,10 @@
                       </tr>
                    </thead>
                    <tbody>
-                     @foreach ($users as $key => $user )
+                    <tr>
+                        <td>No Data</td>
+                    </tr>
+                     {{-- @foreach ($users as $key => $user )
                      <tr>
                         <td>{{ $key+1 }}</td>
                         <td>{{ $user->name }}</td>
@@ -67,7 +70,7 @@
                         <td></td>
                         <td></td>
                     </tr>  
-                     @endforeach
+                     @endforeach --}}
                     
                       
                    </tbody>
