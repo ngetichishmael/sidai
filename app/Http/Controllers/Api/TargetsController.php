258622f9a0934cb5e88fa3280922b97b6bb15fda
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-11 10:18:40 +0300

    Updates to targets

--- app/Http/Controllers/Api/TargetsController.php
+++ app/Http/Controllers/Api/TargetsController.php
@@ -3,28 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TargetResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class TargetsController extends Controller
 {
-    public function countPendingOrders(): int
+    public function getSalespersonTarget(Request $request)
     {
-        return $this->PendingOrders()->where('order_type', 'Pre Order')->whereIn('order_status', ['Pending Derivery', 'Waiting acceptance'])->count();
+        $user_code = $request->user()->user_code;
+        $target = User::with('TargetSale', 'TargetLead', 'TargetOrder', 'TargetVisit')
+            ->where('user_code', $user_code)->first();
+        return response()->json([
+            "success" => true,
+            "message" => "Target Set",
+            "Targets" => new TargetResource($target),
+        ]);
     }
-   public function getSalespersonTarget(Request $request)
-   {
-       $user_code=$request->user()->user_code;
-       $target=User::with('PendingOrders','TargetSales','TargetLeads','TargetsOrder','TargetsVisit')
-       ->where('user_code',$user_code)->get();
-
-       $target->each(function ($item) {
-           $item->pendingOrderCount = $item->countPendingOrders();
-       });
-       return response()->json([
-           "success" => true,
-           "message" => "Target Set",
-           "Targets"=>$target,
-       ]);
-   }
 }
---
+++ app/Http/Resources/TargetResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TargetResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $month = Carbon::now()->endOfMonth()->format('Y-m-d');
+        return [
+            'id' => $this->id,
+            'user_code' => $this->user_code,
+            'name' => $this->name,
+            'email' => $this->email,
+            'target_sale' => $this->TargetSale ? [
+                'SalesTarget' => $this->TargetSale->SalesTarget,
+                'AchievedSalesTarget' => $this->TargetSale->AchievedSalesTarget,
+                'Deadline' => $this->TargetSale->Deadline,
+            ] : [
+                'SalesTarget' => 0,
+                'AchievedSalesTarget' => 0,
+                'Deadline' => $month,
+            ],
+            'target_lead' => $this->TargetLead ? [
+                'LeadsTarget' => $this->TargetLead->LeadsTarget ?? 0,
+                'AchievedLeadsTarget' => $this->TargetLead->AchievedLeadsTarget ?? 0,
+                'Deadline' => $this->TargetLead->Deadline,
+            ] : [
+                'LeadsTarget' => 0,
+                'AchievedLeadsTarget' => 0,
+                'Deadline' => $month,
+            ],
+            'targets_order' => $this->TargetOrder ? [
+                'OrdersTarget' => $this->TargetOrder->OrdersTarget ?? 0,
+                'AchievedOrdersTarget' => $this->TargetOrder->AchievedOrdersTarget ?? 0,
+                'Deadline' => $this->TargetsOrder->Deadline,
+            ] : [
+                'OrdersTarget' => 0,
+                'AchievedOrdersTarget' => 0,
+                'Deadline' => $month,
+            ],
+            'targets_visit' => $this->TargetVisit ? [
+                'VisitsTarget' => $this->TargetVisit->VisitsTarget ?? 0,
+                'AchievedVisitsTarget' => $this->TargetVisit->AchievedVisitsTarget ?? 0,
+                'Deadline' => $this->TargetVisit->Deadline,
+            ] : [
+                'VisitsTarget' => 0,
+                'AchievedVisitsTarget' => 0,
+                'Deadline' => $month,
+            ],
+        ];
+    }
+}
