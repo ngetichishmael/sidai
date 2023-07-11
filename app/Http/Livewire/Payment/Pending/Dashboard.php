e95664dd0b0f17c63cd9d71187e77c826b44acda
Daniel <wisedaniel080@gmail.com>
2023-07-11 15:12:00 +0300

    updates

--- app/Http/Livewire/Payment/Pending/Dashboard.php
+++ app/Http/Livewire/Payment/Pending/Dashboard.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Livewire\Payment\Pending;
 
-use App\Models\Delivery;
+use App\Models\Region;
 use Livewire\Component;
+use App\Models\Delivery;
+use App\Models\customers;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Component
 {
@@ -17,15 +20,43 @@ class Dashboard extends Component
    public $customer_name = null;
    public $name = null;
    public $order_code = null;
+   public $user;
+
+   public function __construct()
+   {
+      $this->user = Auth::user();
+   }
    public function render()
    {
       $deliveries = Delivery::with(['Customer', 'Order', 'User'])
          ->where('delivery_status', 'pending')
          ->orWhere('delivery_status', 'Partial delivery')
+         ->when($this->user->account_type === "RSM",function($query){
+            $query->whereIn('customer', $this->filter());
+         })
          ->orderBy('id', 'desc')
          ->paginate($this->perPage);
       return view('livewire.payment.pending.dashboard', [
          'deliveries' => $deliveries
       ]);
+   }
+   public function filter(): array
+   {
+
+      $array = [];
+      $user = Auth::user();
+      $user_code = $user->region_id;
+      if (!$user->account_type === 'RSM') {
+         return $array;
+      }
+      $regions = Region::where('id', $user_code)->pluck('id');
+      if ($regions->isEmpty()) {
+         return $array;
+      }
+      $customers = customers::whereIn('region_id', $regions)->pluck('id');
+      if ($customers->isEmpty()) {
+         return $array;
+      }
+      return $customers->toArray();
    }
 }
