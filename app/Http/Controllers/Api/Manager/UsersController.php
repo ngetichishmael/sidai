1e2858121c9336fbebc70373d6a7c87ad53bb1ec
ngetichishmael <ngetichishmael786@gmail.com>
2023-07-17 22:46:40 +0300

    Updates to orders

--- app/Http/Controllers/Api/Manager/UsersController.php
+++ app/Http/Controllers/Api/Manager/UsersController.php
@@ -7,7 +7,6 @@ use App\Http\Resources\UserResource;
 use App\Models\suppliers\suppliers;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -33,110 +32,121 @@ class UsersController extends Controller
 //      ]);
 //   }
 
-   public function getUsers(Request $request)
-   {
-      if ($request->account_type == 'RSM') {
-         $users = UserResource::collection(
-            User::withCount('Customers')->with(
-               [
-                  'TargetSales', 'TargetLeads', 'TargetsOrder', 'TargetsVisit'
-               ]
-            )->whereIn('account_type', ['TSR', 'TD', 'Shop-Attendee'])->where('region_id', '=', $request->user()->region_id)->get()
-         );
-      }else{
-         $users = UserResource::collection(
-            User::withCount('Customers')->with(
-               [
-                  'TargetSales', 'TargetLeads', 'TargetsOrder', 'TargetsVisit'
-               ]
-            )->whereIn('account_type', ['TSR', 'TD', 'Shop-Attendee'])->get()
-         );
-      }
-      return response()->json([
-         "success" => true,
-         "status" => 200,
-         "data" => $users,
-      ]);
-   }
-   public function usersList(Request $request)
-   {
-      $accountType = $request->input('account_type');
-
-      if ($request->account_type == 'RSM' || 'rsm') {
-
-         $users =User::where('account_type',$accountType )->whereNotIn('account_type', ['Customer', 'Admin'])->where('status', 'Active')->where('region_id', $request->user()->region_id)
-               ->select('name', 'user_code','account_type')->get();
-         return response()->json([
+    public function getUsers(Request $request)
+    {
+        if ($request->account_type == 'RSM') {
+            $users = UserResource::collection(
+                User::withCount('Customers')->with(
+                    [
+                        'TargetSales', 'TargetLeads', 'TargetsOrder', 'TargetsVisit',
+                    ]
+                )->whereIn('account_type', ['TSR', 'TD', 'Shop-Attendee'])->where('region_id', '=', $request->user()->region_id)->get()
+            );
+        } else {
+            $users = UserResource::collection(
+                User::withCount('Customers')->with(
+                    [
+                        'TargetSales', 'TargetLeads', 'TargetsOrder', 'TargetsVisit',
+                    ]
+                )->whereIn('account_type', ['TSR', 'TD', 'Shop-Attendee'])->get()
+            );
+        }
+        return response()->json([
             "success" => true,
             "status" => 200,
             "data" => $users,
-         ]);
-      }else if ($request->account_type == 'NMS' || 'nsm'){
-         $users = User::where('account_type',$accountType )->whereNotIn('account_type', ['Customer', 'Admin'])->where('status', 'Active')->select('name', 'user_code', 'account_type')->get();
-         return response()->json([
+        ]);
+    }
+    public function usersList(Request $request)
+    {
+        $accountType = $request->input('account_type');
+        $route_code = $request->User()->route_code;
+
+        if ($request->account_type == 'RSM' || 'rsm') {
+
+            $users = User::where('account_type', $accountType)
+                ->whereNotIn('account_type', ['Customer', 'Admin'])
+                ->where('status', 'Active')
+                ->where('route_code', $route_code)
+                ->select('name', 'user_code', 'account_type')->get();
+            return response()->json([
+                "success" => true,
+                "status" => 200,
+                "data" => $users,
+            ]);
+        } else if ($request->account_type == 'NMS' || 'nsm') {
+            $users = User::where('account_type', $accountType)
+                ->whereNotIn('account_type', ['Customer', 'Admin'])
+                ->where('status', 'Active')
+                ->where('route_code', $route_code)
+                ->select('name', 'user_code', 'account_type')->get();
+            return response()->json([
+                "success" => true,
+                "status" => 200,
+                "data" => $users,
+            ]);
+        } else {
+            return response()->json([
+                "success" => false,
+                "status" => 401,
+                "data" => "UNAUTHORIZED USER!!!",
+            ]);
+
+        }
+
+    }
+    public function accountTypes()
+    {
+        $account_types = User::whereNotIn('account_type', ['Customer', 'Admin'])
+            ->select('account_type')
+            ->groupBy('account_type')
+            ->get()
+            ->pluck('account_type')
+            ->toArray();
+
+        $account_types[] = 'Distributors';
+        return response()->json([
             "success" => true,
             "status" => 200,
-            "data" => $users,
-         ]);
-      }
-      return response()->json([
-         "success" => false,
-         "status" => 401,
-         "data" => "UNAUTHORIZED USER!!!",
-      ]);
-   }
-   public function accountTypes()
-   {
-      $account_types = User::whereNotIn('account_type', ['Customer', 'Admin'])
-         ->select('account_type')
-         ->groupBy('account_type')
-         ->get()
-         ->pluck('account_type')
-         ->toArray();
+            "account_types" => $account_types,
 
-      $account_types[] = 'Distributors';
-         return response()->json([
+        ]);
+    }
+    public function distributors()
+    {
+        $distributors = suppliers::whereRaw('LOWER(name) NOT IN (?, ?)', ['sidai', 'sidai'])->whereIn('status', ['Active', 'active'])
+            ->orWhereNull('status')
+            ->orWhere('status', '')
+            ->orderby('name', 'desc')->get();
+        return response()->json([
             "success" => true,
             "status" => 200,
-      "account_types" => $account_types,
+            "distributors" => $distributors,
 
-         ]);
-   }
-   public function distributors()
-   {
-      $distributors = suppliers::whereRaw('LOWER(name) NOT IN (?, ?)', ['sidai', 'sidai'])->whereIn('status', ['Active', 'active'])
-      ->orWhereNull('status')
-      ->orWhere('status', '')
-      ->orderby('name', 'desc')->get();
-      return response()->json([
+        ]);
+    }
+    public function suspendUser(Request $request)
+    {
+        $suspension = User::where('user_code', $request->user_code)->update([
+            'status' => 'suspended',
+        ]);
+        return response()->json([
             "success" => true,
             "status" => 200,
-      "distributors" => $distributors,
-
-         ]);
-   }
-   public function suspendUser(Request $request)
-   {
-      $suspension = User::where('user_code', $request->user_code)->update([
-         'status' => 'suspended',
-      ]);
-      return response()->json([
-         "success" => true,
-         "status" => 200,
-         "message" => "User suspended successfully",
-         "suspension" => $suspension,
-      ]);
-   }
-   public function activateUser(Request $request)
-   {
-      $suspension = User::where('user_code', $request->user_code)->update([
-         'status' => 'Active',
-      ]);
-      return response()->json([
-         "success" => true,
-         "status" => 200,
-         "message" => "User activated successfully",
-         "suspension" => $suspension,
-      ]);
-   }
+            "message" => "User suspended successfully",
+            "suspension" => $suspension,
+        ]);
+    }
+    public function activateUser(Request $request)
+    {
+        $suspension = User::where('user_code', $request->user_code)->update([
+            'status' => 'Active',
+        ]);
+        return response()->json([
+            "success" => true,
+            "status" => 200,
+            "message" => "User activated successfully",
+            "suspension" => $suspension,
+        ]);
+    }
 }
