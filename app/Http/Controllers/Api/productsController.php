bd7e18bf3bed357305eecb439fa0079e14f8ffcd
steven-maina <stevenmaina17@gmail.com>
2023-07-18 10:24:01 +0300

    role issue

--- app/Http/Controllers/Api/productsController.php
+++ app/Http/Controllers/Api/productsController.php
@@ -76,7 +76,6 @@ class productsController extends Controller
          "success" => true,
          "message" => "Product List",
          "data" => $products,
-         "region" => $region->name,
       ]);
    }
    public function index2(Request $request, $warehouseCode)
