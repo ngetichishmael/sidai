08be6a51940fbffa063061d11cba49469cfbe858
Daniel <wisedaniel080@gmail.com>
2023-08-10 08:11:58 +0300

    updates

--- app/Http/Livewire/Dashboard/Dashboard.php
+++ app/Http/Livewire/Dashboard/Dashboard.php
@@ -118,7 +118,7 @@ class Dashboard extends Component
                     });
             })
             ->where(function (Builder $query) {
-                $this->whereBetweenDate($query, 'updated_at', Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth());
+                $this->whereBetweenDate($query, 'updated_at', $this->startDate, $this->endDate);
             })
             ->count();
     }
