6ce20460e4cd2c257e2ab66843ad3efcb012cf0e
Daniel <wisedaniel080@gmail.com>
2023-08-12 08:30:22 +0300

    updates

--- config/chatify.php
+++ config/chatify.php
@@ -6,7 +6,7 @@ return [
     | Messenger display name
     |-------------------------------------
     */
-    'name' => env('CHATIFY_NAME', 'Chat'),
+    'name' => env('CHATIFY_NAME', 'Chatify Messenger'),
 
     /*
     |-------------------------------------
