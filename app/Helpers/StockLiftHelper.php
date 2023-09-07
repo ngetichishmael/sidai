3dee38c00caf01388fb2e64f5e672ed4537b9593
steven-maina <stevenmaina17@gmail.com>
2023-09-07 17:11:45 +0300

    Accet stock status

--- app/Helpers/StockLiftHelper.php
+++ app/Helpers/StockLiftHelper.php
@@ -59,7 +59,7 @@ class StockLiftHelper
          ],
          [
             "business_code" => $business_code,
-            "status" => "Acceptanced",
+            "status" => "Accepted",
             "distributor"=>$distributor,
             "created_by" => $user_code,
             "updated_by" => $user_code
--- app/Http/Controllers/Api/StockRequisitionController.php
+++ app/Http/Controllers/Api/StockRequisitionController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\StockLiftHelper;
 use App\Http\Controllers\Controller;
+use App\Models\inventory\allocations;
+use App\Models\inventory\items;
 use App\Models\products\product_information;
 use App\Models\products\product_inventory;
 use App\Models\RequisitionProduct;
 use App\Models\StockRequisition;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 use Illuminate\Support\Facades\Auth;
@@ -134,14 +137,53 @@ class StockRequisitionController extends Controller
                 ];
 
                 $stocked = product_inventory::find($product->product_id);
-                StockLiftHelper::updateOrCreateItems(
-                    $user_code,
-                    $business_code,
-                    $value,
-                    $image_path,
-                    $random,
-                    $stocked
-                );
+//                StockLiftHelper::updateOrCreateItems(
+//                    $user_code,
+//                    $business_code,
+//                    $value,
+//                    $image_path,
+//                    $random,
+//                    $stocked
+//                );
+               $currentQty = $stocked ? $stocked['current_stock'] : 0;
+               $allocatedQty = $value['qty'];
+               info("quantity       ". $allocatedQty);
+               info(" current qty   ". $allocatedQty);
+               items::updateOrCreate(
+                  [
+                     'product_code' => $value['productID'],
+                     'created_by' => $user_code
+                  ],
+                  [
+                     'business_code' => $business_code,
+                     'allocation_code' => $random,
+                     'current_qty' => $currentQty,
+                     'allocated_qty' => DB::raw('allocated_qty + '.$allocatedQty),
+                     'image' => $image_path,
+                     'returned_qty' => 0,
+                     'created_by' => $user_code,
+                     'updated_by' => $user_code
+                  ]
+               );
+
+               product_inventory::where('productID', $value['productID'])
+                  ->decrement('current_stock', $allocatedQty);
+
+               allocations::updateOrCreate(
+                  [
+                     "allocation_code" => $random,
+                     "sales_person" => $user_code,
+
+                  ],
+                  [
+                     "business_code" => $business_code,
+                     "status" => "Accepted",
+                     "distributor"=>$request->distributor ?? 1,
+                     "created_by" => $user_code,
+                     "updated_by" => $user_code
+                  ]
+               );
+
             }
         }
         return response()->json([
