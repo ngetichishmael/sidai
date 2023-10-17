e9c85389fbf345b2bfe25c8429114c2cb2b25fa7
steven-maina <stevenmaina17@gmail.com>
2023-10-17 16:04:15 +0300

    warehouse products catalogue debug

--- app/Http/Controllers/Api/productsController.php
+++ app/Http/Controllers/Api/productsController.php
@@ -27,6 +27,7 @@ class productsController extends Controller
       $region_id = $request->user()->region_id;
       $warehouse=warehousing::where('region_id', $region_id)->get();
       $region = Region::whereId($region_id)->first();
+      info($warehouse);
    $products = product_information::whereIn('warehouse_code', [$warehouse->warehouse_code])
        ->join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
       ->join('product_price', 'product_price.productID', '=', 'product_information.id')
