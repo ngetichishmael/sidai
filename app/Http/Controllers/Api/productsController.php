36abf79c0b836da9ac1df73677258680d447ba49
steven-maina <stevenmaina17@gmail.com>
2023-10-17 16:29:43 +0300

    warehouse products catalogue debug

--- app/Http/Controllers/Api/productsController.php
+++ app/Http/Controllers/Api/productsController.php
@@ -57,6 +57,64 @@ class productsController extends Controller
          "data" => $products,
       ]);
    }
+   public function index(Request $request, $businessCode)
+   {
+      $route_code = $request->user()->route_code;
+      $region_id = $request->user()->region_id;
+      $region = Region::where(function ($query) use ($route_code, $region_id) {
+         $query->where('id', $route_code)
+            ->orWhere('id', $region_id);
+      })->first();
+      if ($region){
+         $warehouses = warehousing::where('region_id', $region->id)->select('warehouse_code')->distinct('warehouse_code')->get();
+      $products = product_information::whereIn('warehouse_code', [$warehouses])->join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
+         ->join('product_price', 'product_price.productID', '=', 'product_information.id')
+         ->select(
+            'product_price.branch_id as region',
+            'product_information.id as productID',
+            'product_information.created_at as date',
+            'product_price.buying_price as wholesale_price',
+            'product_price.selling_price as retail_price',
+            'product_price.distributor_price as distributor_price',
+            'product_information.product_name as product_name',
+            'product_inventory.current_stock as stock',
+            'product_information.created_at as date',
+            'product_information.business_code as business_code',
+            'sku_code',
+            'brand',
+            'category',
+            'warehouse_code'
+         ) ->groupBy('product_information.product_name', 'product_information.sku_code')
+         ->get();
+   }
+   else
+{
+   $products = product_information::join('product_inventory', 'product_inventory.productID', '=', 'product_information.id')
+      ->join('product_price', 'product_price.productID', '=', 'product_information.id')
+      ->select(
+         'product_price.branch_id as region',
+         'product_information.id as productID',
+         'product_information.created_at as date',
+         'product_price.buying_price as wholesale_price',
+         'product_price.selling_price as retail_price',
+         'product_price.distributor_price as distributor_price',
+         'product_information.product_name as product_name',
+         'product_inventory.current_stock as stock',
+         'product_information.created_at as date',
+         'product_information.business_code as business_code',
+         'sku_code',
+         'brand',
+         'category',
+         'warehouse_code'
+      )
+      ->get();
+}
+      return response()->json([
+         "success" => true,
+         "message" => "All Regional Warehouses Product List",
+         "data" => $products,
+      ]);
+   }
    public function index3(Request $request, $businessCode)
    {
       $route_code = $request->user()->route_code;
