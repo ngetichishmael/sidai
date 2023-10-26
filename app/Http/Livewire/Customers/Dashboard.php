609f20f2bc96f9c5a165cfdda4c870050c95d042
steven-maina <stevenmaina17@gmail.com>
2023-10-26 07:53:21 +0300

    debugging date

--- app/Http/Livewire/Customers/Dashboard.php
+++ app/Http/Livewire/Customers/Dashboard.php
@@ -91,7 +91,7 @@ class Dashboard extends Component
 //             $query->whereDate('created_at', '<=', $endDate);
 //          })
           ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc');
-dd($aggregate->created_at);
+dd($aggregate->select('customer_name','customers.created_at'));
        return $aggregate->paginate($this->perPage);
     }
     public function customers()
