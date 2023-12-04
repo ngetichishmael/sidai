dc2d2c3b99b56a97689ec9b25a61d3853f809917
steven-maina <stevenmaina17@gmail.com>
2023-12-05 01:29:29 +0300

    managers reconciliation approval

--- app/Http/Livewire/Orders/distributororders.php
+++ app/Http/Livewire/Orders/distributororders.php
@@ -85,18 +85,18 @@ class distributororders extends Component
 
       if ($user->account_type ==="Shop-Attendee"){
          $region=warehouse_assign::where('manager', $user->user_code)->first();
-         if ($region->isEmpty()) {
+         if (empty($region)) {
             return $array;
          }
          $customers = customers::whereIn('region_id', $region)->pluck('id');
       }else {
          $regions = Region::where('id', $user_code)->pluck('id');
-         if ($regions->isEmpty()) {
+         if (empty($regions)) {
             return $array;
          }
          $customers = customers::whereIn('region_id', $regions)->pluck('id');
       }
-      if ($customers->isEmpty()) {
+      if (empty($customers)) {
          return $array;
       }
       return $customers->toArray();
