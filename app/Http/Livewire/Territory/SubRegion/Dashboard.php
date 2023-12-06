ff7ff6d38c4d70956235e3ccdf84b068e062669a
steven-maina <stevenmaina17@gmail.com>
2023-12-06 14:24:18 +0300

    managers territory section updates

--- app/Http/Livewire/Territory/SubRegion/Dashboard.php
+++ app/Http/Livewire/Territory/SubRegion/Dashboard.php
@@ -23,10 +23,11 @@ class Dashboard extends Component
       $subregions = Subregion::orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc');
       if ($user->account_type ==="Shop-Attendee") {
          $warehouse = warehouse_assign::where('manager', $user->user_code)->first();
-         if ($warehouse)
-         $warehouse_c=warehousing::where('warehouse_code', $warehouse->warehouse_code)->first();
-         if ($warehouse_c)
-         $subregions->where('region_id',$warehouse_c->region_id);
+         if ($warehouse){
+            $warehouse_c=warehousing::where('warehouse_code', $warehouse->warehouse_code)->first();
+         }if ($warehouse_c){
+            $subregions->where('region_id',$warehouse_c->region_id);
+         }
       }
       $subregions->paginate($this->perPage);
       $customer_counts =customers::where('status','=','Active')->get();
