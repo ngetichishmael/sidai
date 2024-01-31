4c350c97a512d606454b77bc2d4fc0b6dd7b9f8b
steven-maina <stevenmaina17@gmail.com>
2024-01-31 14:58:27 +0300

    approve customer by warehsue manager debug

--- app/Http/Livewire/Customers/Approve.php
+++ app/Http/Livewire/Customers/Approve.php
@@ -137,11 +137,11 @@ class Approve extends Component
             return $array;
          }
          $region = warehousing::where('warehouse_code', $warehouse->warehouse_code)->pluck('region_id');
-         dd($region);
          $customers = customers::where(function ($query) use ($region, $user) {
             $query->whereIn('region_id', $region)
                ->orWhere('created_by', $user->user_code);
          })->pluck('id');
+         dd($customers);
          return $customers->toArray();
       }else {
          $regions = Region::where('id', $user_code)->pluck('id');
