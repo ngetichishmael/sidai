b5189c82f74e0da440c7d29a9acab5a5c1d58d6f
Mugi-Daniel <wisedaniel080@gmail.com>
2024-03-13 22:26:27 +0300

    updates on user sms

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -192,6 +192,73 @@ class AuthController extends Controller
          );
       }
    }
+   
+   public function sendUserSMS($code, $phone_number)
+   {
+      $curl = curl_init();
+      $url = 'https://accounts.jambopay.com/auth/token';
+      curl_setopt($curl, CURLOPT_URL, $url);
+
+      curl_setopt(
+         $curl,
+         CURLOPT_HTTPHEADER,
+         array(
+            'Content-Type: application/x-www-form-urlencoded',
+         )
+      );
+
+      curl_setopt(
+         $curl,
+         CURLOPT_POSTFIELDS,
+         http_build_query(
+            array(
+               'grant_type' => 'client_credentials',
+               'client_id' => "qzuRm3UxXShEGUm2OHyFgHzkN1vTkG3kIVGN2z9TEBQ=",
+               'client_secret' => "36f74f2b-0911-47a5-a61b-20bae94dd3f1gK2G2cWfmWFsjuF5oL8+woPUyD2AbJWx24YGjRi0Jm8="
+            )
+         )
+      );
+
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      $curl_response = curl_exec($curl);
+
+      $token = json_decode($curl_response);
+      curl_close($curl);
+
+      $message = 'Your verification code is ' . $code;
+
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+         CURLOPT_URL => 'https://swift.jambopay.co.ke/api/public/send',
+         CURLOPT_RETURNTRANSFER => true,
+         CURLOPT_ENCODING => '',
+         CURLOPT_MAXREDIRS => 10,
+         CURLOPT_TIMEOUT => 0,
+         CURLOPT_FOLLOWLOCATION => true,
+         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+         CURLOPT_CUSTOMREQUEST => 'POST',
+         CURLOPT_POSTFIELDS => json_encode(
+            array(
+               "sender_name" => "PASANDA",
+               "contact" => $phone_number,
+               "message" => $message,
+               "callback" => "https://pasanda.com/sms/callback"
+            )
+         ),
+
+         CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $token->access_token
+         ),
+      ));
+
+      $response = curl_exec($curl);
+      curl_close($curl);
+      return $response;
+   }
+
     // public function sendOTP($number)
     // {
 
